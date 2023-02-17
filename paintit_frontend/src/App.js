a530c5efa0637693c96dba201f928f550938f51c
Aman Gupta <techbugaman@gmail.com>
2023-02-17 23:11:21 +0530

    Enabled vercel analytics

--- paintit_frontend/src/App.js
+++ paintit_frontend/src/App.js
@@ -1,8 +1,10 @@
+import { inject } from '@vercel/analytics';
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 
 function App() {
+  inject();
   return (
     <>
       <Navbar />
